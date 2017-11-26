0527788e63cab7fb74009d730b61636266d512d5
Jeremy Kauffman <kauffj@gmail.com>
2017-11-26 11:53:42 -0500

    add FileInfo type definition

--- ui/js/types/FileInfo.js
+++ ui/js/types/FileInfo.js
@@ -2,4 +2,27 @@
 
 /*This should actually be an enumeration of all the fields on file data type*/
 
-export type FileInfo = {};
+export type FileInfo = {
+  channel_name: ?string,
+  claim_id: string,
+  completed: boolean,
+  download_directory: string,
+  download_path: string,
+  file_name: string,
+  has_signature: boolean,
+  key: string,
+  message: ?string,
+  metadata: {},
+  mime_type: string,
+  name: string,
+  outpoint: string,
+  points_paid: number,
+  sd_hash: string,
+  signature_is_valid: boolean,
+  stopped: boolean,
+  stream_hash: string,
+  stream_name: string,
+  suggested_file_name: string,
+  total_bytes: ?number,
+  written_bytes: number,
+};
